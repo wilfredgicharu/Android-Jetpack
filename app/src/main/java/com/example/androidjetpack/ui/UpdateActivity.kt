433f602a389d3c51d5f535307548e2eb6f76074d
wilfred gicharu <wilfredgcharu254@gmail.com>
2022-10-29 23:54:33 +0300

    adding room

--- app/src/main/java/com/example/androidjetpack/ui/UpdateActivity.kt
+++ app/src/main/java/com/example/androidjetpack/ui/UpdateActivity.kt
@@ -1,29 +1,79 @@
 package com.example.androidjetpack.ui
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import com.example.androidjetpack.Model.Note
 import com.example.androidjetpack.R
 import com.example.androidjetpack.ViewModel.NoteViewModel
 import com.example.androidjetpack.databinding.ActivityUpdateBinding
-import java.util.Date
+import java.util.*
 
 class UpdateActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivityUpdateBinding
+    @SuppressLint("RestrictedApi")
+    private lateinit var date:Date
+    private lateinit var  getNote:Note
     private lateinit var noteViewModel: NoteViewModel
-    private lateinit var getNote: Note
-    private lateinit var date: Date
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_update)
+        getDate()
+        noteViewModel= NoteViewModel()
+        updateCharacter.text="| 0 characters"
+        updateNote.addTextChangedListener(textWatcher)
 
-        binding = ActivityUpdateBinding.inflate(layoutInflater)
+        val bundle:Bundle?=intent.extras
+        if(bundle != null){
+            getNote = bundle.getSerializable("note") as Note
+        }
+        loadNote(getNote)
+        updateData.setOnClickListener {
+            updateNote()
+        }
+    }
 
-        setContentView(binding.root)
+    private fun loadNote(note :Note)
+    {
+        updateNote.setText(note.data)
+        updateCharacter.text="${note.characters}"
+    }
 
+    private fun updateNote()
+    {
+        getNote.data=updateNote.text.toString()
+        getNote.date=updateDate.text.toString()
+        getNote.characters=updateNote.text.toString().length.toLong()
+        noteViewModel.update(applicationContext,getNote)
+        val intent= Intent(this,MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private val textWatcher= object : TextWatcher {
+        override fun afterTextChanged(s: Editable?) {
+
+        }
+
+        override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+        }
+
+        override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+            updateCharacter.text= " | Character "+s?.length.toString()
+
+        }
 
     }
+
+
+    private fun getDate() {
+        date= Calendar.getInstance().time
+        updateDate.text=date.toString()
+    }
+
+
+
 }
